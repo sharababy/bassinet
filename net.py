07a16af1bbaf75c40ef498bc44c99bf76ab75980
Vedant Bassi <bassi.vdt@gmail.com>
2018-05-02 21:25:45 +0530

    made class for neural net

--- net.py
+++ net.py
@@ -12,17 +12,40 @@ classes = ["airplane","automobile","bird","cat","deer","dog","frog","horse","shi
 
 class NeuralNet:
 
-	def trainThis(image,self):
-		print(self[3])
+	def __init__(self):
+		self.layer = [np.random.ranf((3072,1)) * np.random.ranf((1,3072)),
+		np.random.ranf((10,1)) * np.random.ranf((1,3072)),
+		np.random.ranf((10,1)) * np.random.ranf((1,10)),
+		np.random.ranf((10,1)) * np.random.ranf((1,10))]	
+
+	def trainThis(self,image,cnumber):
+		out1 = np.matmul(self.layer[0] , image)
+
+		out1 = [x/10000 for x in out1]
+		out2 = np.matmul(self.layer[1] , out1)
+		out3 = np.matmul(self.layer[2] , out2)
+		out4 = np.matmul(self.layer[3] , out3)
+
+		# print(out1)
+		# print(out2)
+		# print(out3)
+		# print(out4)
+
+		errors = 
+
+		return out4;
+
+	def printLayer(self,n):
+		print(self.layer[n])
 
 
-net =[np.random.randint(0,1000,(3072,1),dtype=np.int64) * np.random.randint(0,1000,(1,3072),dtype=np.int64),
-np.random.randint(0,1000,(10,1),dtype=np.int64) * np.random.randint(0,1000,(1,3072),dtype=np.int64),
-np.random.randint(0,1000,(10,1),dtype=np.int64) * np.random.randint(0,1000,(1,10),dtype=np.int64),
-np.random.randint(0,1000,(10,1),dtype=np.int64) * np.random.randint(0,1000,(1,10),dtype=np.int64)]
+net = NeuralNet();
+
+flayer = net.trainThis(imgset[b'data'][0]);
 
 
 
+# print(flayer)
 
 
 '''
@@ -30,6 +53,8 @@ np.random.randint(0,1000,(10,1),dtype=np.int64) * np.random.randint(0,1000,(1,10
 # to print any label 
 [print(classes[imgset[b'labels'][i]]) for i in range(10)]
 
+# tp print image data
+# print(imgset[b'data'])
 
 [3072 x 3072][3072 x 1] = [3072 x 1]   -- layer 1
 
